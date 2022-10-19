337f10d4a0f0e04d1d55cf54823d479a29f9d66c
Fabricio Marques <fabricio@mundialeditora.com>
2022-10-19 14:34:21 -0300

    Estilização do Layout e Componente: Header

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,12 +1,13 @@
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
 import { Outlet } from 'react-router-dom'
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
     return (
-        <div>
+        <LayoutContainer>
             <Header />
 
             <Outlet /> {/* Conteúdo das páginas */}
-        </div>
+        </LayoutContainer>
     )
 }
